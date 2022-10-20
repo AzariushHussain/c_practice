fb7389dd703c766810c3e66953e3daa6bb89c5d6
Azariush Hussain <88018508+AzariushHussain@users.noreply.github.com>
2022-10-20 11:58:44 +0530

    it calculates multiplication tables of large numbers using string

---
+++ tables_using_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+void print_table(char *str_num)
+{
+  char **result = (char **)malloc(10 * sizeof(char*));
+  for (int j = 1; j <=10; j++)
+  {
+        result[j]=(char*)malloc(strlen(str_num)*sizeof(char));
+  for (int i = strlen(str_num) - 1; i >= 0; i++)
+    {
+        int calc=((int)str_num[i]-48);
+        int carry=calc/10;
+        result[j][i]=(char)(calc%10+carry*10);
+    
+  }
+  result=realloc(result,strlen(str_num));
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%s x %d = %s\n",str_num,i+1,result[i]);
+  }
+  
+}
+int main(int argc, char const *argv[])
+{
+  char *str_num;
+  printf("Enter the number whose table is to be printed .\n");
+  scanf("%s", str_num);
+  str_num = realloc(str_num, strlen(str_num));
+  print_table(str_num);
+
+  return 0;
+}
